07c66973acce40e13009eeae8adc6e27d8e2442a
10nnn <218318377+10nnn@users.noreply.github.com>
2026-02-04 14:50:52 +0900

    ルーティング作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,17 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AdminContactController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [ContactController::class, 'index']);           // 入力
+Route::post('/confirm', [ContactController::class, 'confirm']); // 確認
+Route::post('/thanks', [ContactController::class, 'store']);     // 保存・完了
+
+Route::get('/admin',  [AdminContactController::class, 'index']);   // 一覧
+Route::get('/search', [AdminContactController::class, 'search']);  // 検索
+Route::get('/reset',  [AdminContactController::class, 'reset']);   // 検索解除
+Route::delete('/delete', [AdminContactController::class, 'destroy']); // 削除
+Route::get('/export', [AdminContactController::class, 'export']);  // CSV等
+
+Route::get('/register',  [AuthController::class, 'showRegister']); //認証
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/login',  [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
